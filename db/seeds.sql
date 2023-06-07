1cf02d94d8e71e447fb8a482bbb5c62f54887fb3
Mona Lisa <madison0327@hotmail.com>
2023-06-07 18:13:21 -0400

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,21 +7,21 @@ VALUES ("HR"),
 
 
 INSERT INTO role (title, department_id, salary)
-VALUES ("Director", "HR", 10000),
-       ("Account Manager", "Finance", 160000),
-       ("Accountant", "Finance", 125000),
-       ("Brand Management", "Marketing", 65000),
-       ("Social Media Marketing", "Marketing", 55000),
-       ("Custom Service Associate", "Customer Service", 45000),
-       ("IT Helpdesk", "Technology", 45000);
+VALUES ("Director", 1, 10000),
+       ("Account Manager", 2, 160000),
+       ("Accountant", 2, 125000),
+       ("Brand Management", 4, 65000),
+       ("Social Media Marketing", 4, 55000),
+       ("Custom Service Associate", 5, 45000),
+       ("IT Helpdesk", 3, 45000);
        
 INSERT INTO employee (last_name, first_name, role_id, manager_id)
-VALUES ("Walker", "Madison"),
-       ("Doe", "John"),
-       ("Chan","Jackie"),
-       ("Sky", "Blue"),
-       ("Grass", "Green"),
-       ("Banana", "Yellow");
+VALUES ("Walker", "Madison", 1,1),
+       ("Doe", "John",3,2),
+       ("Chan","Jackie",4,3),
+       ("Sky", "Blue",2,null),
+       ("Grass", "Green",4,5),
+       ("Banana", "Yellow",4,null);
 
 
        
