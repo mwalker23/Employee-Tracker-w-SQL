44f3f92ceb6cb3e9d98fac6f7f6fbcd90fd032de
Mona Lisa <madison0327@hotmail.com>
2023-06-10 21:18:27 -0400

    updated readme and index.js

--- index.js
+++ index.js
@@ -2,85 +2,140 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      password: '',
-      database: 'departments_db'
-    },
-    console.log(`Connected to the departments_db database.`)
-  ); 
-  
-function mainMenu(){
-inquirer.prompt ([
-    {
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    password: "",
+    database: "departments_db",
+  },
+  console.log(`Connected to the departments_db database.`)
+);
+
+function mainMenu() {
+  inquirer
+    .prompt([
+      {
         type: "list",
         name: "menuChoices",
         message: "What would you like to view?",
-        choices: ["view all departments","add a department", "view all roles", "add a role", 
-        "update an employee role","view all employees","add an employee"]
-    }
-]) .then (answers => {
-    if (answers.menuChoices === "view all departments" ) {
-        viewAllDep ()
-    }
-   
-        if (answers.menuChoices === "view all roles" ) {
-            viewAllRoles ()
-        }
+        choices: [
+          "view all departments",
+          "add a department",
+          "view all roles",
+          "add a role",
+          "update an employee role",
+          "view all employees",
+          "add an employee",
+        ],
+      },
+    ])
+    .then((answers) => {
+      if (answers.menuChoices === "view all departments") {
+        viewAllDep();
+      }
+      if (answers.menuChoices === "add a department") {
+        addDep();
+      }
 
-        if (answers.menuChoices === "view all employees" ) {
-            viewAllEmp ()
-        }
-})
-};
+      if (answers.menuChoices === "view all roles") {
+        viewAllRoles();
+      }
+      if (answers.menuChoices === "add a role") {
+        addRole();
+      }
 
-
-function viewAllDep (){
-const viewDep = ` SELECT * FROM departments`;
-db.query(viewDep, (err,results) => {
-if (err) throw err;
-console.table(results);
-})
+      if (answers.menuChoices === "update an employee") {
+        newEmpInfo();
+      }
+      if (answers.menuChoices === "view all employees") {
+        viewAllEmp();
+      }
+      if (answers.menuChoices === "add an employee") {
+        addEmp();
+      }
+    });
 }
 
-
-function viewAllRoles (){
-    const viewRoles = ` SELECT * FROM role`;
-    db.query(viewRoles, (err,results) => {
+function viewAllDep() {
+  const viewDep = ` SELECT * FROM departments`;
+  db.query(viewDep, (err, results) => {
     if (err) throw err;
     console.table(results);
-    })
-    }
+    mainMenu();
+  });
+}
 
-    
-    function viewAllEmp (){
-        const viewEmployees = ` SELECT * FROM employee`;
-        db.query(viewEmployees, (err,results) => {
-        if (err) throw err;
-        console.table(results);
-        })
+function addDep() {
+    inquirer
+    .prompt ([
+        {
+            type: "input",
+            name: "department_name",
+            message: "What is the name of the Department?",
         }
-        
+    ]) .then ((answers) => {
+        const newDep= ` INSERT INTO departments SET ?`
+        db.query(newDep, answers, (err,results) => {
+            if (err) throw err;
+            console.table(results);
+            mainMenu();
+        })
+    } )
+}
 
-mainMenu ();
+function viewAllRoles() {
+  const viewRoles = ` SELECT * FROM role`;
+  db.query(viewRoles, (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    mainMenu();
+  });
+}
 
+function addRole() {
+    inquirer
+    .prompt ([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the name of the Role?",
+        }
+    ]) .then ((answers) => {
+        const newRole= ` INSERT INTO role SET ?`
+        db.query(newRole, answers, (err,results) => {
+            if (err) throw err;
+            console.table(results);
+            mainMenu();
+        })
+    } )
+}
 
-// THEN I am presented with the following options: view all departments, 
-// view all roles, view all employees, add a department, add a role, 
-// add an employee, and update an employee role
+function viewAllEmp() {
+  const viewEmployees = ` SELECT * FROM employee`;
+  db.query(viewEmployees, (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    mainMenu();
+  });
+}
 
-// const sql = `INSERT INTO movies (movie_name)
-//     VALUES (?)`;
-//   const params = [body.movie_name];
-  
-//   db.query(sql, params, (err, result) => {
-//     if (err) {
-//       res.status(400).json({ error: err.message });
-//       return;
-//     }
+function addEmp() {
+    inquirer
+    .prompt ([
+        {
+            type: "input",
+            name: "first_name" + "last_name" + "",
+            message: "What is the employees name?",
+        }
+    ]) .then ((answers) => {
+        const newEmp = `INSERT INTO employee SET ?`
+        db.query(newEmp, answers, (err, results) => {
+            if (err) throw err;
+            console.table(results);
+            mainMenu();
+    })
+  });
+};
 
-// console.table(result);
-
-//   });
+mainMenu();
